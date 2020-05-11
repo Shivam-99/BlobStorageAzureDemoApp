9fd8a1b7d16eb17de24f3a027026400d566fb974
Shivam-99 <37024449+Shivam-99@users.noreply.github.com>
2020-05-11 12:48:55 +0530

    Add Project Files

---
+++ BlobAppDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+namespace BlobAppDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string localPath = "./BlobFiles/";
+            Console.WriteLine("Enter the file name ");
+            string fileName = Console.ReadLine() + Guid.NewGuid().ToString() + ".txt";
+
+            Console.WriteLine("Enter the content to be entered and press Enter  ");
+            string content = Console.ReadLine();
+
+            UploadFile(fileName, localPath, content);
+            Console.ReadLine();
+        }
+
+        static async void UploadFile(string fileName,string FilePath,string content)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            string fullPath = Path.Combine(FilePath, fileName);
+            ;
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            string containerName = "RandomBlobTest" + Guid.NewGuid();
+            BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+            await File.WriteAllTextAsync(FilePath, content);
+
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+            using FileStream uploadFileStream = File.OpenRead(fullPath);
+            await blobClient.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+        }
+    }
+}
